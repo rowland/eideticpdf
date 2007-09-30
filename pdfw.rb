6be17d65191624a165b5bc13864a3753cee3b8f8
brent <brent@499c8170-6e11-0410-a2ac-93a16911f373>
2007-09-30 23:17:32 +0000

    Now can create documents with blank pages!
    git-svn-id: https://one.eidetic.us/svn/eidetic/trunk/libraries/pdfw@190 499c8170-6e11-0410-a2ac-93a16911f373

--- pdfw.rb
+++ pdfw.rb
@@ -6,6 +6,16 @@
 require 'pdfu'
 
 module PdfW
+  UNIT_CONVERSION = { :pt => 1, :in => 72, :cm => 28.35 }
+
+  Location = Struct.new(:x, :y)
+
+  def convert_units(loc, from_units, to_units)
+    Location.new(
+      loc.x * UNIT_CONVERSION[from_units] / UNIT_CONVERSION[to_units],
+      loc.y * UNIT_CONVERSION[from_units] / UNIT_CONVERSION[to_units])
+  end
+
   def f(value, prec=2)
     sprintf("%.*f", prec, value).sub(',','.')
   end
@@ -16,8 +26,8 @@ module PdfW
   end
 
   class PageStyle
-    attr_reader :page_size, :crop_size, :orientation, :landscape
-    
+    attr_reader :page_size, :crop_size, :orientation, :landscape, :rotate
+
     PORTRAIT = 0
     LANDSCAPE = 270
 
@@ -28,6 +38,7 @@ module PdfW
       @page_size = make_size_rectangle(page_size, @orientation)
       @crop_size = make_size_rectangle(crop_size, @orientation)
       @landscape = (@orientation == :landscape)
+      @rotate = ROTATIONS[@orientation]
     end
 
   private
@@ -53,13 +64,14 @@ module PdfW
         :landscape => [0,0,649,459].freeze
       }.freeze
     }
+    ROTATIONS = { :portrait => PORTRAIT, :landscape => LANDSCAPE }.freeze
 
   protected
     def make_size_rectangle(size, orientation)
       PdfU::Rectangle.new(*(SIZES[size][orientation]))
     end
   end
-  
+
   class MiscWriter
     def initialize(stream)
       @stream = stream
@@ -114,7 +126,7 @@ module PdfW
       @stream << "/#{name} Do\n"
     end
   end
-  
+
   class GraphWriter
     def initialize(stream)
       @stream = stream
@@ -299,38 +311,12 @@ module PdfW
     end
   end
 
-  class PdfWriter
+  class PdfPageWriter
   private
     def arc_small(x, y, r, mid_theta, half_angle, ccwcw, move_to0)
     end
 
     def set_text_angle(angle, x, y)
-    end
-
-  protected
-    def next_seq
-    end
-
-    def define_resources
-    end
-
-    def make_font_descriptor(font_name)
-    end
-
-    def check_set_line_dash_pattern
-    end
-
-    def check_set_line_width
-    end
-
-    # color methods
-    def check_set_line_color
-    end
-
-    def check_set_fill_color
-    end
-
-    def check_set_font_color
     end
 
   protected
@@ -351,7 +337,7 @@ module PdfW
 
     def end_misc
     end
-    
+
     attr_reader :tw, :gw
 
     def page_width
@@ -360,36 +346,62 @@ module PdfW
     def page_height
     end
 
+    # color methods
+    def check_set_line_color
+    end
+
+    def check_set_fill_color
+    end
+
+    def check_set_font_color
+    end
+
   public
-    # currently, should only be called once
-    def write(stream)
+    attr_reader :doc, :units
+    attr_reader :stream, :annotations
+
+    def initialize(doc, options)
+      # doc: PdfDocumentWriter
+      @doc = doc
+      @page_style = PageStyle.new(options)
+      @units = options[:units] || :pt
+      @page_width = @page_style.page_size.x2
+      @page_height = @page_style.page_size.y2
+      @page = PdfU::PdfPage.new(@doc.next_seq, 0, @doc.catalog.pages)
+      @page.media_box = @page_style.page_size.clone
+      @page.crop_box = @page_style.crop_size.clone
+      @page.rotate = @page_style.rotate
+      @page.resources = @doc.resources
+      @doc.file.body << @page
+      @stream = ''
+      @annotations = []
+      start_misc
     end
 
-    # document methods
-    def begin_doc(options={})
+    def close
+      end_text if @in_text
+      end_graph if @in_graph
+      end_misc if @in_misc
+      pdf_stream = PdfU::PdfStream.new(@doc.next_seq, 0, @stream)
+      @doc.file.body << pdf_stream
+      @page.annots = @annotations if @annotations.size.nonzero?
+      @page.contents << pdf_stream
+      @doc.catalog.pages.kids << @page
+      @stream = nil
     end
 
-    def end_doc
-    end
-
-    # page methods
-    def start_page(options={})
-    end
-
-    def end_page
-    end
-
-    def new_page(options={})
+    def closed?
+      @stream.nil?
     end
 
     # coordinate methods
-    def units
-    end
-
     def units=(units)
+      @loc = convert_units(@loc, @units, units)
+      @last_loc = convert_units(@last_loc, @units, units)
+      @units = units
     end
 
-    def moveTo(x, y)
+    def move_to(x, y)
     end
 
     def pen_pos
@@ -505,15 +517,248 @@ module PdfW
 
     def print_link(s, uri)
     end
+  end
+
+  class PdfDocumentWriter
+    def next_seq
+      @next_seq += 1
+    end
+
+    attr_reader :cur_page, :pages
+    attr_reader :catalog, :file, :resources
 
     # instantiation
     def initialize
+      @fonts = {}
+      @images = {}
+      @encodings = {}
     end
-    
-    def open
+
+    def open(options={})
     end
-    
+
     def close
+    end
+
+    def to_s
+      @file.to_s
+    end
+
+    # document methods
+    def begin_doc(options={})
+      raise Exception.new("Already in document") if @in_doc
+      @in_doc = true
+      @options = options
+      @pages = []
+      @next_seq = 0
+      @file = PdfU::PdfFile.new
+      pages = PdfU::PdfPages.new(next_seq, 0)
+      outlines = PdfU::PdfOutlines.new(next_seq, 0)
+      @catalog = PdfU::PdfCatalog.new(next_seq, 0, :use_none, pages, outlines)
+      @file.body << pages << outlines << @catalog
+      @file.trailer.root = @catalog
+      define_resources
+    end
+
+    def end_doc
+      @pages.each { |page| page.close unless page.closed? }
+    end
+
+    # page methods
+    def start_page(options={})
+      raise Exception.new("Already in page") if @in_page
+      @cur_page = PdfPageWriter.new(self, @options.clone.update(options))
+      @pages << @cur_page
+      @in_page = true
+    end
+
+    def end_page
+      raise Exception.new("Not in page") unless @in_page
+      @cur_page.close
+      @cur_page = nil
+      @in_page = false
+    end
+
+    def new_page(options={})
+    end
+    
+    # coordinate methods
+    def units
+      cur_page.units
+    end
+
+    def units=(units)
+      cur_page.units=(units)
+    end
+
+    def move_to(x, y)
+      cur_page.move_to(x, y)
+    end
+
+    def pen_pos
+      cur_page.pen_pos
+    end
+
+    # graphics methods
+    def line_to(x, y)
+      cur_page.line_to(x, y)
+    end
+
+    def rectangle(x, y, width, height, border=true, fill=false)
+      cur_page.rectangle(x, y, width, height, border, fill)
+    end
+
+    def curve(x0, y0, x1, y1, x2, y2, x3, y3)
+      cur_page.curve(x0, y0, x1, y1, x2, y2, x3, y3)
+    end
+
+    def curve_points(points)
+      cur_page.curve_points(points)
+    end
+
+    def curve_to(points)
+      cur_page.curve_to(points)
+    end
+
+    def circle(x, y, r, border=true, fill=false)
+      cur_page.circle(x, y, r, border, fill)
+    end
+
+    def arc(x, y, r, start_angle, end_angle, move_to0=false)
+      cur_page.arc(x, y, r, start_angle, end_angle, move_to0)
+    end
+
+    def pie(x, y, r, start_angle, end_angle, border=true, fill=false)
+      cur_page.pie(x, y, r, start_angle, end_angle, border, fill)
+    end
+
+    def arch(x, y, r1, r2, start_angle, end_angle, border=true, fill=false)
+      cur_page.arch(x, y, r1, r2, start_angle, end_angle, border, fill)
+    end
+
+    def fill
+      cur_page.fill
+    end
+
+    def stroke
+      cur_page.stroke
+    end
+
+    def fill_and_stroke
+      cur_page.fill_and_stroke
+    end
+
+    def set_line_dash_pattern(pattern, phase)
+      cur_page.set_line_dash_pattern(pattern, phase)
+    end
+
+    def set_line_width(line_width)
+      cur_page.set_line_width(line_width)
+    end
+
+    # color methods
+    def set_line_color_rgb(red, green, blue)
+      cur_page.set_line_color_rgb(red, green, blue)
+    end
+
+    def set_line_color(color)
+      cur_page.set_line_color(color)
+    end
+
+    def set_fill_color_rgb(red, green, blue)
+      cur_page.set_fill_color_rgb(red, green, blue)
+    end
+
+    def set_fill_color(color)
+      cur_page.set_fill_color(color)
+    end
+
+    def set_font_color_rgb(red, green, blue)
+      cur_page.set_font_color_rgb(red, green, blue)
+    end
+
+    def set_font_color(color)
+      cur_page.set_font_color(color)
+    end
+
+    # text methods
+    def print(text, angle=0.0)
+      cur_page.print(text, angle)
+    end
+
+    def print_xy(x, y, text, angle=0.0)
+      cur_page.print_xy(x, y, text, angle)
+    end
+
+    def puts(text='')
+      cur_page.puts(text)
+    end
+
+    def puts_xy(x, y, text)
+      cur_page.puts_xy(x, y, text)
+    end
+
+    def width(text)
+      cur_page.width(text)
+    end
+
+    def height # may not include external leading?
+      cur_page.height
+    end
+
+    def set_v_text_align(vta)
+      cur_page.set_v_text_align(vta)
+    end
+
+    # font methods
+    def set_font(name, size, options = {})
+      cur_page.set_font(name, size, options)
+    end
+
+    def set_font_style(style)
+      cur_page.set_font_style(style)
+    end
+
+    def set_font_size(size)
+      cur_page.set_font_size(size)
+    end
+
+    # image methods
+    def load_image(image_file_name)
+      cur_page.load_image(image_file_name)
+    end
+
+    def print_image_handle(pdf_image_handle, x, y, width=nil, height=nil)
+      cur_page.print_image_handle(pdf_image_handle, x, y, width, height)
+    end
+
+    def print_image_file(image_file_name, x, y, width=nil, height=nil)
+      cur_page.print_image_file(image_file_name, x, y, width, height)
+    end
+
+    def print_link(s, uri)
+      cur_page.print_link(s, uri)
+    end
+
+  protected
+    def define_resources
+      @resources = PdfU::PdfResources.new(next_seq, 0)
+      @resources.proc_set = PdfU::PdfArray.new [
+        PdfU::PdfName.new('PDF'),
+        PdfU::PdfName.new('Text'),
+        PdfU::PdfName.new('ImageB'),
+        PdfU::PdfName.new('ImageC')
+      ]
+      @file.body << @resources
+    end
+
+    def make_font_descriptor(font_name)
+    end
+
+    def check_set_line_dash_pattern
+    end
+
+    def check_set_line_width
     end
   end
 end
